ab029a7f0774046d9985a22b95305efcf5b143d2
Mika Ratilainen <mihail.ratilainen@yandex.ru>
2020-03-05 00:55:56 +0300

    added cors

---
+++ middlewares/cors.js
@@ -0,0 +1,20 @@
+const allowedCors = [
+  'https://ratilainen.ru',
+  'http://ratilainen.ru',
+  'http://localhost:8080',
+];
+
+const corsChecker = (req, res, next) => {
+  const { origin } = req.headers;
+
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+      res.status(200);
+    }
+
+    next();
+};
+
+module.exports = { corsChecker };
