b54badda88c824770401050d6e09df7682b107b1
Mika Ratilainen <mihail.ratilainen@yandex.ru>
2020-03-05 23:24:37 +0300

    changed token logic

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,8 @@ const auth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(req.cookies.jwt, SECRET_KEY);
+    const token = req.header('authorization');
+    payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
     throw new UnauthorizedError(ERR_NOT_SIGNED_IN);
   }
